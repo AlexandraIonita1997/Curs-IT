024bb5a97204f07e1838e2efdc1f3fd68050f7ff
AlexandraIonita1997 <105743496+AlexandraIonita1997@users.noreply.github.com>
2022-05-17 21:19:45 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -16,19 +16,19 @@ showSlides();
 
 function showSlides() {
   let i;
-  let slides = document.getElementsByClassName("desene");
-  let dots = document.getElementsByClassName("punct");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  let desene = document.getElementsByClassName("desene");
+  let puncte = document.getElementsByClassName("punct");
+  for (i = 0; i < desene.length; i++) {
+    desene[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {
+  if (slideIndex > desene.length) {
     slideIndex = 1;
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  for (i = 0; i < puncte.length; i++) {
+    puncte[i].className = puncte[i].className.replace(" active", "");
   }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+  desene[slideIndex - 1].style.display = "block";
+  puncte[slideIndex - 1].className += " active";
   setTimeout(showSlides, 5000);
 }
